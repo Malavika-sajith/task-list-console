5a4b5c61370ed09ed1128e62d7470706c1d7567d
Malavika Sajith <haibinu46@gmail.com>
2026-09-06 17:09:59 +0200

    Add functionalith to create new tasks via console menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Program started");
+        ArrayList<Task> tasks = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        int nextId = 1;
+
+        while (running) {
+            System.out.println("\n---Task Manager---");
+            System.out.println("1. Add Task");
+            System.out.println("2. View Task");
+            System.out.println("3. Mark Task completed");
+            System.out.println("4. Exit");
+            System.out.print("choose an option: ");
+
+            int choice = scanner.nextInt();
+            if(choice==1) {
+                scanner.nextLine();// clear leftover input
+                System.out.print("Enter task title:");
+                String title = scanner.nextLine();
+
+                tasks.add(new Task(nextId, title));
+                nextId++;
+                System.out.println("Task added!");
+            }else if (choice == 4){
+                running = false;
+                System.out.println("GoodBye!");
+            }
+        }
+
+
     }
 }
